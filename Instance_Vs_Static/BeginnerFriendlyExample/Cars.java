9305d261a8f47077a1aa592679257a27ea14a535
Min Zayar Maung <minzayarmaung2002@gmail.com>
2024-08-05 22:13:29 +0630

    instance_vs_static methods

--- Instance_Vs_Static/BeginnerFriendlyExample/Cars.java
+++ Instance_Vs_Static/BeginnerFriendlyExample/Cars.java
@@ -4,10 +4,13 @@ public class Cars {
 
     // Instance variable
     private String brand;
+    // Static variable
+    private static int numberOfCars;
 
     // Constructor
     public Cars(String brand) {
         this.brand = brand;
+        numberOfCars++;
     }
 
     // Instance Method
@@ -15,9 +18,23 @@ public class Cars {
         System.out.println("The Brand of the Car is: " + brand);
     }
 
+    // Static Method
+    public static void displayNumberOfCars() {
+        System.out.println("Number of Car: " + numberOfCars);
+    }
+
     public static void main(String[] args) {
+        // Need to Create a new instances
         Cars myCar = new Cars("Toyota");
+        Cars myCar1 = new Cars("Honda");
+        Cars myCar2 = new Cars("Mercedes");
+
         myCar.displayBrand();
+        myCar1.displayBrand();
+        myCar2.displayBrand();
+
+        // No need to Create an Instance
+        Cars.displayNumberOfCars(); // Output will be 3
     }
 
 }
