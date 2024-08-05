be032be14c5ff61d1c13653e21d730b9320468c8
Min Zayar Maung <minzayarmaung2002@gmail.com>
2024-08-05 21:48:59 +0630

    instance_vs_static methods

---
+++ Instance_Vs_Static/Instance_Vs_Static_Method.java
@@ -0,0 +1,8 @@
+package Instance_Vs_Static;
+
+public class Instance_Vs_Static_Method {
+    
+    // Links to Read Resources
+    String link1 = "https://stackoverflow.com/questions/11993077/difference-between-static-methods-and-instance-methods";
+    
+}
