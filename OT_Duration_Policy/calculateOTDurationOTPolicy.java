417016a85457377747485658d3ddf682419b6bbf
Min Zayar Maung <minzayarmaung2002@gmail.com>
2024-08-08 22:15:45 +0630

    OTDurationCalculation

--- OT_Duration_Policy/calculateOTDurationOTPolicy.java
+++ OT_Duration_Policy/calculateOTDurationOTPolicy.java
@@ -1,6 +1,35 @@
 package OT_Duration_Policy;
 
 public class calculateOTDurationOTPolicy {
-    
+
+    String assignStartTime = "090000";
+    String assignEndTime = "173000";
+
+    public static String calculateOTforAssignDays(String OTstartTime, String OTendTime, String assignStartTime,
+            String assignEndTime) {
+
+        double start = Double.parseDouble(assignStartTime);
+        double end = Double.parseDouble(assignEndTime);
+
+        double otStart = Double.parseDouble(OTstartTime);
+        double otEnd = Double.parseDouble(OTendTime);
+
+        if (start >= otStart && otEnd <= end) {
+
+        } else {
+            return "0 hours";
+        }
+
+        return "10 hours";
+    }
+
+    public static String calculateOTforOffDays(double startTime, double endTime) {
+
+        return "10 days";
+    }
+
+    public static void main(String[] args) {
+
+    }
 
 }
