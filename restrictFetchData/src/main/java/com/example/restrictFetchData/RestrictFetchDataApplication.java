1fe4013fe2db4f82991e4459ad51ebe468817852
Min Zayar Maung <minzayarmaung2002@gmail.com>
2024-08-14 21:38:40 +0630

    restrict login

---
+++ restrictFetchData/src/main/java/com/example/restrictFetchData/RestrictFetchDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.restrictFetchData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestrictFetchDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestrictFetchDataApplication.class, args);
+	}
+
+}
---
+++ restrictFetchData/src/test/java/com/example/restrictFetchData/RestrictFetchDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restrictFetchData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestrictFetchDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
