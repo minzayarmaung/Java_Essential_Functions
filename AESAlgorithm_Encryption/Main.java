7984ce9f984daf7eff3c0c4b515b9bd38138ec0e
Min Zayar Maung <minzayarmaung2002@gmail.com>
2024-09-02 22:30:42 +0630

    aes algorithm

---
+++ AESAlgorithm_Encryption/Main.java
@@ -0,0 +1,13 @@
+package AESAlgorithm_Encryption;
+
+public class Main {
+    public static void main(String[] args) {
+        String plainText = "Hello, this is a secret message.";
+        String encryptedString = AESAlgorithm.encryptString(plainText);
+
+        String decryptedString = AESAlgorithm.decryptString(encryptedString);
+
+        System.out.println("Encrypted Value : " + encryptedString);
+        System.out.println("Decrypted Value :" + decryptedString);
+    }
+}
