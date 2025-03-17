b0dac60c8c66ea403fe4c509aff7b541c3e08131
Min Zayar Maung <minzayarmaung2002@gmail.com>
2025-03-17 22:19:35 +0630

    add new

---
+++ Singleton_Implementation/Singleton_Test.java
@@ -0,0 +1,19 @@
+public class Singleton_Test {
+
+    public static void main(String[] args) {
+        Abc obj1 = Abc.getInstance();
+        Abc obj2 = Abc.getInstance();
+    }
+
+    class Abc {
+        static Abc obj = new Abc();
+
+        private Abc() {
+
+        }
+
+        public static Abc getInstance() {
+            return obj;
+        }
+    }
+}
